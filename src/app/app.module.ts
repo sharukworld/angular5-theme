4841cc4a5f428ba2cc4a9a01b1725bfa7d8bb1ad
Sharuk Ahmed Mazumder <sharuk.ahmed@coda.global>
2018-01-15 20:33:21 +0530

    Fixed depedencies issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NbThemeModule } from '@nebular/theme';
+import { NbLayoutModule, NbSidebarModule, NbSidebarService, NbThemeModule } from '@nebular/theme';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardModule } from './dashboard/dashboard.module';
@@ -8,14 +9,17 @@ import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([{ path: '', component: DashboardComponent}]),
     NbThemeModule.forRoot({ name: 'default' }),
-    DashboardModule
+    NbLayoutModule,
+    NbSidebarModule
   ],
-  providers: [],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
